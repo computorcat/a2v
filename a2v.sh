163aaa49c1e048b947d5302cd48d989153bc6d16
computorcat <hannahtisback@gmail.com>
2023-08-07 18:56:39 +0100

    s2v

--- a2v.sh
+++ a2v.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 function a2v(){
     mkdir -p output
+    audio="$1"
+    image="$2"
+    dimensions="$3"
+    # if #1 and #2 and #3 are empty, ask for them
+    if [ -z "$audio" ] && [ -z "$image" ] && [ -z "$dimensions" ]; then
+        read -rp "Enter the audio data type (e.g., mp3, wav, ogg): " audio
+        read -rp "Enter the album art file name (e.g., image.jpg): " image
 
-    read -rp "Enter the audio data type (e.g., mp3, wav, ogg): " audio
-    read -rp "Enter the album art file name (e.g., image.jpg): " image
+        if [ ! -f "$image" ]; then
+            echo "File not found!"
+            sleep 0.5
+            a2v
+        fi
 
-    if [ ! -f "$image" ]; then
-        echo "File not found!"
-        sleep 0.5
-        a2v
+        read -rp "Enter the video dimensions (e.g., 1920x1080): " dimensions
     fi
-
-    read -rp "Enter the video dimensions (e.g., 1920x1080): " dimensions
-
     # if dimensions is empty, set to 1920x1080
     if [ -z "$dimensions" ]; then
         vw=1920
@@ -27,9 +31,37 @@ function a2v(){
     pause 
 
     for audio_file in *."$audio"; do
-        ffmpeg -loop 1 -i "$image" -i "$audio_file" -c:a copy -c:v libx264 -vf "scale=$vw:$vh:force_original_aspect_ratio=decrease,pad=$vw:$vh:(ow-iw)/2:(oh-ih)/2,setsar=1" -shortest "output/${audio_file%.*}.mp4"
+        ffmpeg -loop 1 -i "$image" -i "$audio_file" -c:a copy -c:v libx264 -pix_fmt yuv420p -vf "scale=$vw:$vh:force_original_aspect_ratio=decrease,pad=$vw:$vh:(ow-iw)/2:(oh-ih)/2,setsar=1" -shortest "output/${audio_file%.*}.mp4"
     done
 }
+
+function s2v(){
+    read -rp "Input a link to a song: " link
+    title=$(yt-dlp --get-title "$link" | head -n 1)
+    if [ $? -ne 0 ]; then
+        echo "Error fetching title from yt-dlp"
+        return 1
+    fi
+
+    clean_title=$(echo "$title" | tr -d '/\:*?"<>|')
+    mkdir -p "$clean_title"
+    cd "$clean_title" || exit
+
+    yt-dlp -x --audio-format mp3 "$link"
+    if [ $? -ne 0 ]; then
+        echo "Error downloading audio with yt-dlp"
+        return 1
+    fi
+
+    wget -O image.jpg "$(yt-dlp --get-thumbnail "$link")"
+    if [ $? -ne 0 ]; then
+        echo "Error downloading image with wget"
+        return 1
+    fi
+
+    a2v mp3 image.jpg 1920x1080
+}
+
 
 while true; do
     clear
